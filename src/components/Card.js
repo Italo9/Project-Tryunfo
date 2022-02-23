b24dc79fd667a2d695c4dd6a9dfc51e17ef59023
ítalo Lima <contatoitallo@hotmail.com>
2022-02-23 17:44:20 -0300

    Cria e renderiza o componente Card com as props necessárias-Req 3

---
+++ src/components/Card.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import PropType from 'prop-types';
+
+class Card extends React.Component {
+  render() {
+    const { cardName,
+      cardDescription,
+      cardAttr1,
+      cardAttr2,
+      cardAttr3,
+      cardImage,
+      cardRare,
+      cardTrunfo,
+    } = this.props;
+
+    return (
+      <main>
+        <div data-testid="name-card">{cardName}</div>
+        <img data-testid="image-card" src={ cardImage } alt={ cardName } />
+        <div data-testid="description-card">{cardDescription}</div>
+        <div data-testid="attr1-card">{cardAttr1}</div>
+        <div data-testid="attr2-card">{cardAttr2}</div>
+        <div data-testid="attr3-card">{cardAttr3}</div>
+        <div data-testid="rare-card">{cardRare}</div>
+        {cardTrunfo
+          ? <div data-testid="trunfo-card">`Super Trunfo`</div>
+          : ''}
+      </main>
+    );
+  }
+}
+
+Card.propTypes = {
+  cardName: PropType.string.isRequired,
+  cardDescription: PropType.string.isRequired,
+  cardAttr1: PropType.string.isRequired,
+  cardAttr2: PropType.string.isRequired,
+  cardAttr3: PropType.string.isRequired,
+  cardImage: PropType.string.isRequired,
+  cardRare: PropType.string.isRequired,
+  cardTrunfo: PropType.bool.isRequired,
+};
+
+export default Card;
